781b97490abe41b069635c978134624976b6c8ac
QairulShazriz <qairulshazriz.b@digipen.edu>
2021-11-21 23:33:31 +0800

    Finalized Camera

--- camera.c
+++ camera.c
@@ -27,31 +27,6 @@ void camera_init(CP_Vector playerPosition)
 void camera_update(CP_Vector playerPosition, CP_Vector playerSize,
 	float timeElapsed)
 {
-	/*if (CP_Input_KeyDown(KEY_UP) || CP_Input_KeyDown(KEY_W))
-	{
-		currentPosition.y += timeElapsed * PAN;
-		translationMatrix = CP_Matrix_Translate(currentPosition);
-		printf("Current Pos: %f %f\n", currentPosition.x, currentPosition.y);
-	}
-	
-
-	else if (CP_Input_KeyDown(KEY_RIGHT) || CP_Input_KeyDown(KEY_D))
-	{
-		currentPosition.x += timeElapsed * -PAN;
-		translationMatrix = CP_Matrix_Translate(currentPosition);
-	}
-
-	else if (CP_Input_KeyDown(KEY_LEFT) || CP_Input_KeyDown(KEY_A))
-	{
-		currentPosition.x += timeElapsed * PAN;
-		translationMatrix = CP_Matrix_Translate(currentPosition);
-		printf("Current Pos: %f %f\n", currentPosition.x, currentPosition.y);
-	}
-	else if (CP_Input_KeyDown(KEY_DOWN))
-	{
-		currentPosition.y += timeElapsed * -PAN;
-		translationMatrix = CP_Matrix_Translate(currentPosition);
-	}*/
 
 	currentPosition.x = -(playerPosition.x + (playerSize.x / 2) - CP_System_GetWindowWidth() / 2.0f);
 	translationMatrix = CP_Matrix_Translate(currentPosition);
--- cameraV2.h
+++
@@ -1,2 +0,0 @@
-void camerav2_init(CP_Vector playerPosition);
-void camerav2_update(CP_Vector playerPosition, float timeElapsed);
